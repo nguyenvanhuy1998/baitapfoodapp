e29de6fbd02c87f16aae696a1998dba1fcb0daa2
Huy Nguyễn <huygemini1998@gmail.com>
2022-05-06 16:40:37 +0700

    no message

--- src/screens/FoodDetail/index.js
+++ src/screens/FoodDetail/index.js
@@ -4,25 +4,41 @@ import {theme} from '../../constants';
 import Header from './components/Header';
 import Detail from './components/Detail';
 import Cart from './components/Cart';
+import {isIphoneX} from 'react-native-iphone-x-helper';
 const FoodDetail = ({route}) => {
   const {item} = route.params;
   const [countFood, setCountFood] = useState(1);
   return (
     <SafeAreaView style={styles.container}>
       <Header name={item.name} />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        alwaysBounceVertical={false}>
-        <Detail
-          setCountFood={setCountFood}
-          countFood={countFood}
-          image={item.photo}
-          name={item.name}
-          price={item.price}
-          cal={item.cal}
-        />
-        <Cart countFood={countFood} price={item.price * countFood} />
-      </ScrollView>
+
+      {isIphoneX() ? (
+        <>
+          <Detail
+            setCountFood={setCountFood}
+            countFood={countFood}
+            image={item.photo}
+            name={item.name}
+            price={item.price}
+            cal={item.cal}
+          />
+          <Cart countFood={countFood} price={item.price * countFood} />
+        </>
+      ) : (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          alwaysBounceVertical={false}>
+          <Detail
+            setCountFood={setCountFood}
+            countFood={countFood}
+            image={item.photo}
+            name={item.name}
+            price={item.price}
+            cal={item.cal}
+          />
+          <Cart countFood={countFood} price={item.price * countFood} />
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
